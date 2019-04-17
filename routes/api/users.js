846c20a6fe883bc1ca4ceeba1c57895e9b4c288b
dienigma <joshi.chinmay09@gmail.com>
2019-04-17 08:52:49 +0530

    Built the user login

--- routes/api/users.js
+++ routes/api/users.js
@@ -48,4 +48,30 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @router  GET api/users/login
+// @desc    login user(returning the token)
+// @access  Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find the user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: "user not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ message: "Success" });
+      } else {
+        return res.status(400).json({ password: "password incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
